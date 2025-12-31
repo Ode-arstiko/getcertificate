a48c5dfff96781fe54481778334b793550882522
Ode Arstiko <dewiharjanto2@gmail.com>
2025-12-31 08:50:46 +0700

    update3

--- resources/views/layouts/admin/head.blade.php
+++ resources/views/layouts/admin/head.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Modernize Free</title>
     <link rel="shortcut icon" type="image/png" href="/template/src/assets/images/logos/favicon.png" />
-    <link rel="stylesheet" href="/template/src/assets/css/styles.min.css" />
+    <link rel="stylesheet" href="{{ asset('template/src/assets/css/styles.min.css') }}" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/5.3.0/fabric.min.js"></script>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
